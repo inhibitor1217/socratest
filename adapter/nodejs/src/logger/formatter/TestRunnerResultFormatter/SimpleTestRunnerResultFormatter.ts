198d9b99b000ef923046c1793e4100905f130296
dugi <dugi@channel.io>
2021-09-14 23:11:45 +0900

    feat: implement SimpleTestRunnerResultFormatter

--- adapter/nodejs/src/logger/formatter/TestRunnerResultFormatter/SimpleTestRunnerResultFormatter.ts
+++ adapter/nodejs/src/logger/formatter/TestRunnerResultFormatter/SimpleTestRunnerResultFormatter.ts
@@ -5,6 +5,21 @@ import type { TestRunnerResultFormatter } from './interface'
 @autobind
 export default class SimpleTestRunnerResultFormatter implements TestRunnerResultFormatter {
   format(result: SocratestTestRunnerResult): string {
-    return 'hello'
+    return this.formatCount(result.count)
+  }
+
+  private formatCount(count: SocratestTestRunnerResult['count']): string {
+    return [this.formatTestCount(count.test), this.formatTestCaseCount(count.testcase)].join('\n')
+  }
+
+  private formatTestCount(test: SocratestTestRunnerResult['count']['test']): string {
+    const { total, executed, pass, fail } = test
+    return `Found ${total} tests (${executed} executed, ${pass} passed, ${fail} failed).`
+  }
+
+  private formatTestCaseCount(testcase: SocratestTestRunnerResult['count']['testcase']): string {
+    const { total, executed, pass, fail } = testcase
+    return `Found ${total} testcases (${executed} executed, ${pass} passed, ${fail} failed).`
+
   }
 }
