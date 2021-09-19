d2178f04f5041eca0dc5382e7d3fe44dacf54475
dugi <dugi@channel.io>
2021-09-19 13:40:57 +0900

    fix: implementation of compose

--- adapter/nodejs/src/util/pipe/compose.ts
+++ adapter/nodejs/src/util/pipe/compose.ts
@@ -1,9 +1,23 @@
 import _ from 'lodash'
 
 export default function compose(obj: any): any {
-  return (param: any) => _.mapValues(obj, (value) => {
-    if (typeof value === 'object') return compose(value)(param)
-    if (typeof value === 'function') return value(param)
-    return value
-  })
+  return (param: any) => {
+    if (obj instanceof Promise) {
+      return obj.then((resolved) => compose(resolved)(param))
+    }
+    
+    if (typeof obj === 'object') {
+      if (_.isArray(obj)) {
+        return _.map(obj, (value) => compose(value)(param))
+      }
+
+      return _.mapValues(obj, (value) => compose(value)(param))
+    }
+
+    if (typeof obj === 'function') {
+      return obj(param)
+    }
+
+    return obj
+  }
 }
